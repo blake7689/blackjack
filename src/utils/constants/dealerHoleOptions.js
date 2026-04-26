835dee4f1d1aeb9321bf653d5456e02aeb0b6a26
blake7689 <blakec7689@gmail.com>
2026-04-27 00:55:09 +0100

    minor

--- src/utils/constants/dealerHoleOptions.js
+++ src/utils/constants/dealerHoleOptions.js
@@ -2,5 +2,5 @@ export const DealerHoleOptions = {
   "Draw Early - No Check_US": "drawEarly_NoCheck",
   "Draw Early - Check_US": "drawEarly_Check",
   "Draw After_EU": "drawAfter",
-  "Draw_After - NoDraw21 EU": "drawAfter",
+  "Draw_After - NoDraw21 EU": "drawAfter_noDraw21",
 };
